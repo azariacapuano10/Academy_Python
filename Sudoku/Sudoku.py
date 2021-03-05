4764729ba4c0f97de280ae5566726467a942c039
azariacapuano10 <azaria.capuano@studio.unibo.it>
2021-03-05 12:17:48 +0100

    Creazione e print matrice

--- Sudoku/Sudoku.py
+++ Sudoku/Sudoku.py
@@ -2,3 +2,13 @@ controllo = list() #creazione controllo
 for i in range(1,10):
     controllo.append(i)
 
+matrice_sudoku = [] #creazione matrice
+for r in range(9):
+    lista = list()
+    matrice_sudoku.append(lista)
+    for c in range(9):
+        num = int()
+        lista.append(num)
+
+for i in range(9): #print matrice
+    print(matrice_sudoku[i])
